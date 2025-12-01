ceffd5e9530d47ba5d971221676f5a6c1f1b8af2
rbnbb <rbnbb@users.noreply.github.com>
2025-12-01 17:59:51 +0100

    feat: #1.2 solved 1st try as well, a bit slow and inelegeant, meh

--- 01/solution.jl
+++ 01/solution.jl
@@ -6,16 +6,30 @@ using DelimitedFiles
 function find_password(rotations::Vector{String})::Integer
     num_times_dial_points_0 = 0
     dial_position = 50  # see instructions
-    println("Dial at $dial_position...")
+    println("  Dial at $dial_position...")
     for rotation in rotations
         println("Applying $rotation...")
         sign = rotation[1] == 'L' ? -1 : +1
         distance = parse(Int, rotation[2:end])
-        dial_position = mod(dial_position + sign * distance, 100)
-        println("Dial at $dial_position...")
+        num_times_dial_points_0 += div(distance, 100)
+        distance = mod(distance, 100)
+        orig_dial = copy(dial_position)
+        dial_position = dial_position + sign * distance  # new integer
+        println("  raw position $dial_position")
+        if sign == -1 && dial_position < 0 && orig_dial != 0  # left rotation
+            num_times_dial_points_0 += 1
+            println("  passing 0 once (leftward)")
+        elseif sign == 1 && dial_position > 100  # right rotation
+            num_times_dial_points_0 += 1
+            println("  passing 0 (rightward)")
+        end
+        println("    nclicks at 0: $num_times_dial_points_0")
+        dial_position = mod(dial_position, 100)
         if dial_position == 0
             num_times_dial_points_0 += 1
+            println("  Dial at 0 (exactly)!")
         end
+        println("  Dial at $dial_position...")
     end
     return num_times_dial_points_0
 end
