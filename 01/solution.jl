2cb3292803940b5cf85ef7d77fc7d6d8f20a6720
rbnbb <rbnbb@users.noreply.github.com>
2025-12-01 18:28:29 +0100

    fix: condense ifs, since doing R100 from 0 overcounts,

--- 01/solution.jl
+++ 01/solution.jl
@@ -3,33 +3,34 @@
 using DelimitedFiles
 
 
+# Subtleties:
+# * if distance > 100 we can have guranteed many wraps around => count dith div
+# * if distance < 100: rightward passes 0 if new_pos >100 & leftward if new_pos < 0
+# * if left turn starting at 0 there is no click => don't count
+
 function find_password(rotations::Vector{String})::Integer
     num_times_dial_points_0 = 0
     dial_position = 50  # see instructions
-    println("  Dial at $dial_position...")
+    # println("  Dial at $dial_position...")
     for rotation in rotations
-        println("Applying $rotation...")
+        # println("Applying $rotation...")
         sign = rotation[1] == 'L' ? -1 : +1
         distance = parse(Int, rotation[2:end])
         num_times_dial_points_0 += div(distance, 100)
         distance = mod(distance, 100)
         orig_dial = copy(dial_position)
         dial_position = dial_position + sign * distance  # new integer
-        println("  raw position $dial_position")
-        if sign == -1 && dial_position < 0 && orig_dial != 0  # left rotation
+        # println("  raw position $dial_position")
+        if sign == -1 && dial_position <= 0 && orig_dial != 0  # left rotation
             num_times_dial_points_0 += 1
-            println("  passing 0 once (leftward)")
-        elseif sign == 1 && dial_position > 100  # right rotation
+            # println("  passing 0 once (leftward)")
+        elseif sign == 1 && dial_position >= 100  # right rotation
             num_times_dial_points_0 += 1
-            println("  passing 0 (rightward)")
+            # println("  passing 0 (rightward)")
         end
-        println("    nclicks at 0: $num_times_dial_points_0")
+        # println("    nclicks at 0: $num_times_dial_points_0")
         dial_position = mod(dial_position, 100)
-        if dial_position == 0
-            num_times_dial_points_0 += 1
-            println("  Dial at 0 (exactly)!")
-        end
-        println("  Dial at $dial_position...")
+        # println("  Dial at $dial_position...")
     end
     return num_times_dial_points_0
 end
